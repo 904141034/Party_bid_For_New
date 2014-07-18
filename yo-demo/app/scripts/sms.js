0568c31f86e180a741ff58cb832028e9f480ecb0
your_name <your_email>
2014-07-18 13:53:57 +0800

    realize the function that the same phonenumber can not be registered

--- yo-demo/app/scripts/sms.js
+++ yo-demo/app/scripts/sms.js
@@ -17,17 +17,19 @@ var native_accessor = {
 
         //提取出活动
         var activities = JSON.parse(localStorage.getItem('activities')) || [];
+
         var InnerAct=JSON.parse(localStorage.getItem('InnerAct'))|| [];
         var mess=json_message.messages[0].message;
         var get_name;
         var get_phone;
         var str="";
+        var ll=0;
         for(var i=0;i<mess.length;i++){
             if(mess.substr(i,1)!=" "){
                 str+=mess.substr(i,1);
             }
         }
-  
+
         if(str.substr(0,2)=="bm"||str.substr(0,2)=="BM"){
             get_name=str.substr(2);//姓名
             get_phone = json_message.messages[0].phone;
@@ -36,17 +38,28 @@ var native_accessor = {
         if(InnerAct.act=="true") {
             for (var j = 0; j < activities.length; j++) {
 
+
                 if (activities[j].name == InnerAct.name) {
-                    //新建一个对象存储报名用户信息
-                    var bmMessage = {};
-                    var bmMessages = activities[j].bmMessages;
-                    bmMessage.person_name = get_name;
-                    bmMessage.phone_number= get_phone;
-                    bmMessages.unshift(bmMessage);
-                    activities[j].bmMessages = bmMessages;
-                    localStorage.setItem("activities", JSON.stringify(activities));
-                    var message="恭喜！报名成功";
-                    this.send_sms(get_phone,message);
+
+                    var actMessages =activities[j].bmMessages;
+                    for(var l=0;l<actMessages.length;l++) {
+                        if (actMessages[l].phone_number != get_phone) {
+                            ll++;
+                        }
+                    }
+                    if(ll==actMessages.length){
+                        //新建一个对象存储报名用户信息
+                        var bmMessage = {};
+                        var bmMessages = activities[j].bmMessages;
+                        bmMessage.person_name = get_name;
+                        bmMessage.phone_number = get_phone;
+                        bmMessages.unshift(bmMessage);
+                        activities[j].bmMessages = bmMessages;
+                        localStorage.setItem("activities", JSON.stringify(activities));
+                        var message = "恭喜！报名成功";
+                        this.send_sms(get_phone, message);
+                    }
+
                 }
 
             }
