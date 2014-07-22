60cab22dd2201702781068ce63a6e5588ee16e24
your_name <your_email>
2014-07-22 14:07:07 +0800

    define the array for the bid information in the localstorage

--- yo-demo/app/scripts/controllers/activity_create.js
+++ yo-demo/app/scripts/controllers/activity_create.js
@@ -3,7 +3,7 @@
  * Created by lishaodan on 14-7-9.
  */
 angular.module('yoDemoApp')
-    .controller('activity_createCtrl', function ($scope,$location) {
+    .controller('activity_createCtrl', function ($scope, $location) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
@@ -13,55 +13,71 @@ angular.module('yoDemoApp')
         //定义activities数组并获取
         var activity = {};
         var activities = JSON.parse(localStorage.getItem('activities')) || [];
-        var InnerAct={};
+        var InnerAct = {};
+
+        var bidmessage = {};
+        var bidmessages = [];
+        var bidlists = [];
+
 
         //返回按钮
-        $scope.show="false";
-        if(activities.length!=0){
-            $scope.show="true";
+        $scope.show = "false";
+        if (activities.length != 0) {
+            $scope.show = "true";
 
-        }else{
-            $scope.show="false";
+        } else {
+            $scope.show = "false";
         }
-        $scope.return=function(){
+        $scope.return = function () {
             $location.path('/activity_list');
         }
 
 
-        $scope.IsUseStr=function(){
+        $scope.IsUseStr = function () {
 
         }
 
         //创建活动
-        $scope.create_Activity=function(){
+        $scope.create_Activity = function () {
             //重复警告
 
             $scope.textAlert = "false";
-                ///判断活动重名
-             for(var i=0;i<activities.length;i++){
-                if($scope.activity_name==activities[i].name){
-                        $scope.textAlert="true";
+            ///判断活动重名
+            for (var i = 0; i < activities.length; i++) {
+                if ($scope.activity_name == activities[i].name) {
+                    $scope.textAlert = "true";
 
                     break;
-                    }
-
                 }
-           if($scope.textAlert=="false"){
 
-            activity.name=$scope.activity_name;
-            activity.bmMessages=[];
-            activity.status="";
-            activities.unshift(activity);
-            localStorage.setItem("activities", JSON.stringify(activities));
+            }
+            if ($scope.textAlert == "false") {
 
-            InnerAct.name=$scope.activity_name;
-            InnerAct.act="false";
-               localStorage.setItem("InnerAct",JSON.stringify(InnerAct));
+                activity.name = $scope.activity_name;
+                activity.bmMessages = [];
+                activity.status = "";
+
+                bidmessage.person_name = "";
+                bidmessage.phone_number = "";
+                bidmessage.price = "";
+                bidmessages.unshift(bidmessage);
+                bidlists.bidmessages = bidmessages;
+                bidlists.bid_name = "";
+                activity.bidlists = bidlists;
+
+                activities.unshift(activity);
+                localStorage.setItem("activities", JSON.stringify(activities));
+               // console.log(activities);
 
 
-            $location.path('/activity_register');
+                InnerAct.name = $scope.activity_name;
+                InnerAct.act = "false";
+                localStorage.setItem("InnerAct", JSON.stringify(InnerAct));
 
-           }
+
+                $location.path('/activity_register');
+
+            }
 
         };
 
--- yo-demo/app/scripts/controllers/bid_list.js
+++ yo-demo/app/scripts/controllers/bid_list.js
@@ -3,7 +3,7 @@
  * Created by lishaodan on 14-7-22.
  */
 angular.module('yoDemoApp')
-    .controller('bid_listCtrl', function ($scope, $location) {
+    .controller('bid_listCtrl', function ($scope) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
