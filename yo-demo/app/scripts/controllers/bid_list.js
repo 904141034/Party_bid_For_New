071056d712a02211f86102b49feeea5c74fd0ac2
your_name <your_email>
2014-07-22 15:03:08 +0800

    finish the function of adding a bid for the third card

--- yo-demo/app/scripts/controllers/bid_list.js
+++ yo-demo/app/scripts/controllers/bid_list.js
@@ -3,10 +3,29 @@
  * Created by lishaodan on 14-7-22.
  */
 angular.module('yoDemoApp')
-    .controller('bid_listCtrl', function ($scope) {
+    .controller('bid_listCtrl', function ($scope, $location) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
+        $scope.start=function(){
+            var activities = JSON.parse(localStorage.getItem("activities")) || [];
+            var InnerAct = JSON.parse(localStorage.getItem("InnerAct")) || [];
+            for(var i=0;i<activities.length;i++){
+                if(InnerAct.name==activities[i].name){
+                    var bidno=activities[i].bidlists.length;
+                    var bid_name="竞价"+(bidno+1);
+                    activities[i].bidlists.bid_name=bid_name;
+                    localStorage.setItem("activities", JSON.stringify(activities));
+                    console.log(activities);
+                }
+            }
+
+        };
+        //返回按钮
+        $scope.return1= function () {
+            $location.path('/activity_list');
+
+        }
     });
