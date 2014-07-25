363af1f7f106d763afe0b9bdd1bc3af16442d462
your_name <your_email>
2014-07-25 14:13:26 +0800

    show the success bid information in the footbar

--- yo-demo/app/scripts/controllers/price_statistics.js
+++ yo-demo/app/scripts/controllers/price_statistics.js
@@ -11,6 +11,7 @@ angular.module('yoDemoApp')
         ];
         var bid_pricegroup=JSON.parse(localStorage.getItem("bid_pricegroup"));
         var InnerAct=JSON.parse(localStorage.getItem("InnerAct"));
+        var bid_success=JSON.parse(localStorage.getItem("bid_success"));
         var No=0;
         for(var i=0;i<bid_pricegroup.length;i++){
             No+=bid_pricegroup[i].count;
@@ -18,4 +19,8 @@ angular.module('yoDemoApp')
         $scope.bidMessageNO=No;
         $scope.bid_name=InnerAct.bid_name;
         $scope.bid_pricegroup=bid_pricegroup;
+
+        $scope.bid_successperson=bid_success.person_name;
+        $scope.bid_successprice=bid_success.bid_price;
+        $scope.bid_successphoneNumber=bid_success.phone_number;
     });
